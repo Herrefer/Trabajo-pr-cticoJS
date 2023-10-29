0fe50f618f291b533613b26f374d3e45aafe534c
Herrera Fernando <herrera770@outlook.es>
2023-10-29 16:40:34 -0300

    se agregó la solucion al problema

---
+++ js/06-jsVariables.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("Ingrese el primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+
+if (numero1 > numero2){
+    alert("El número mayor es " + numero1)
+}else{
+    alert("El número mayor es " + numero2)
+}
