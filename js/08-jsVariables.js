a6ec6da95edf7eda172a2bfa14d0cfc60d07f05a
Herrera Fernando <herrera770@outlook.es>
2023-10-29 17:11:37 -0300

    se agregó la solucion al problema

---
+++ js/08-jsVariables.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("Ingrese un número"));
+
+if (numero1 % 2 == 0){
+    document.write (numero1 + " es divisible en 2")
+}else{
+    document.write (numero1 + " no es divisible en 2")
+}
