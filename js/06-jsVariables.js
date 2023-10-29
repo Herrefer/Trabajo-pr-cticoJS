5ed10308f7205fc4a4a5c5dd555fb52b5257d406
Herrera Fernando <herrera770@outlook.es>
2023-10-29 16:45:28 -0300

    los resultados del ejercicio 6 ahora se imprimen en la pantalla

--- js/06-jsVariables.js
+++ js/06-jsVariables.js
@@ -2,7 +2,7 @@ let numero1 = parseInt(prompt("Ingrese el primer número"));
 let numero2 = parseInt(prompt("Ingrese el segundo número"));
 
 if (numero1 > numero2){
-    alert("El número mayor es " + numero1)
+    document.write("El número mayor es " + numero1)
 }else{
-    alert("El número mayor es " + numero2)
+    document.write("El número mayor es " + numero2)
 }
